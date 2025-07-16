62e902df634deea2fcee4d7b2d34a8b89783dee6
User <stavskaatatna2@gmail.com>
2025-07-16 23:53:38 +0300

    черепаха

--- code.cf.py
+++ code.cf.py
@@ -4,7 +4,7 @@ import turtle
 def f(x):
     return 100 * math.sqrt(abs(1 - 0.01 * x ** 2)) + 0.01 * abs(x + 10)
 
-# Настройки окна
+
 screen = turtle.Screen()
 screen.setup(800, 600)
 screen.title("График функции")
@@ -13,7 +13,7 @@ t = turtle.Turtle()
 t.speed(0)
 t.penup()
 
-# Рисуем оси
+
 t.goto(-300, 0)
 t.pendown()
 t.goto(300, 0)
@@ -23,12 +23,12 @@ t.pendown()
 t.goto(0, 200)
 t.penup()
 
-# Рисуем график
+
 x = -15
-t.goto(x * 20, f(x) - 200)  # Масштабируем координаты
+t.goto(x * 20, f(x) - 200)
 t.pendown()
 while x <= 5:
-    t.goto(x * 20, f(x) - 200)  # Смещаем график вниз
+    t.goto(x * 20, f(x) - 200)
     x += 0.1
 
 turtle.done()
